103bce20f85c111995623aa13dbb9b67680cf492
portalguitarshred <portalguitarshred@gmail.com>
2024-05-19 17:18:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.getElementById('audio-player');
 
     const stations = [
-        { name: 'Rock Guitar', url: 'https://stream-ssl.shoutcast.com/classicrock' },  // URL direta de um stream de áudio
-        { name: 'Jazz Guitar', url: 'http://stream-url-jazz' },
-        { name: 'Blues Guitar', url: 'http://stream-url-blues' },
-        { name: 'Heavy Metal Guitar', url: 'http://stream-url-metal' },
-        { name: 'Instrumental Guitar', url: 'http://stream-url-instrumental' }
+        { name: 'Rock Guitar', url: 'https://stream-uk1.radioparadise.com/aac-320' }, // URL direta de um stream de áudio de Rock
+        { name: 'Jazz Guitar', url: 'http://stream-url-jazz' }, // Substitua por uma URL real de Jazz
+        { name: 'Blues Guitar', url: 'http://stream-url-blues' }, // Substitua por uma URL real de Blues
+        { name: 'Heavy Metal Guitar', url: 'http://stream-url-metal' }, // Substitua por uma URL real de Heavy Metal
+        { name: 'Instrumental Guitar', url: 'http://stream-url-instrumental' } // Substitua por uma URL real de Instrumental
     ];
 
     stations.forEach(station => {
