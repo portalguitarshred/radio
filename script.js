6e1daa5bf7dcc9e07f4e2c3f20a3e51a82d95f4d
portalguitarshred <portalguitarshred@gmail.com>
2024-05-19 16:50:58 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const stationList = document.getElementById('station-list');
+    const audioPlayer = document.getElementById('audio-player');
+
+    const stations = [
+        { name: 'Rock Guitar', url: 'http://stream-url-rock' },
+        { name: 'Jazz Guitar', url: 'http://stream-url-jazz' },
+        { name: 'Blues Guitar', url: 'http://stream-url-blues' },
+        { name: 'Heavy Metal Guitar', url: 'http://stream-url-metal' },
+        { name: 'Instrumental Guitar', url: 'http://stream-url-instrumental' }
+    ];
+
+    stations.forEach(station => {
+        const li = document.createElement('li');
+        li.textContent = station.name;
+        li.addEventListener('click', () => {
+            audioPlayer.src = station.url;
+            audioPlayer.play();
+        });
+        stationList.appendChild(li);
+    });
+});
