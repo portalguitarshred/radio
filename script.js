21590368578bbfe3a14fff7565cb9cf840d0eba9
portalguitarshred <portalguitarshred@gmail.com>
2024-05-19 17:20:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.getElementById('audio-player');
 
     const stations = [
-        { name: 'Rock Guitar', url: 'https://stream-uk1.radioparadise.com/aac-320' }, // URL direta de um stream de áudio de Rock
+        { name: 'Rock Guitar', url: 'https://rockfm-ice.stream.goloudservices.com/rockfm.mp3' }, // URL direta de um stream de áudio de Rock
         { name: 'Jazz Guitar', url: 'http://stream-url-jazz' }, // Substitua por uma URL real de Jazz
         { name: 'Blues Guitar', url: 'http://stream-url-blues' }, // Substitua por uma URL real de Blues
         { name: 'Heavy Metal Guitar', url: 'http://stream-url-metal' }, // Substitua por uma URL real de Heavy Metal
