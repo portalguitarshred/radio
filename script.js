b89f5a19445c7d10d4ff670a8807091a673fde09
portalguitarshred <portalguitarshred@gmail.com>
2024-05-20 00:57:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'Rock', url: 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?1716174521095' }, // URL funcional para Rock
         { name: 'Jazz', url: 'https://server01.ouvir.radio.br:8006/stream?1716175071041' },
         { name: 'Blues', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
-        { name: 'Gospel', url: 'https://stm39.stmsrv.com:8382/;?1716176724313' },
+        { name: 'Metal', url: 'https://stm39.stmsrv.com:8382/;?1716176724313' },
         { name: 'Anos 80', url: 'https://live1.livemus.com.br:27400/stream?1716176895555' }
     ];
 
