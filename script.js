9e7627cad0b9451325c9b997f24d633090804aa7
portalguitarshred <portalguitarshred@gmail.com>
2024-05-20 00:51:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.getElementById('audio-player');
 
     const stations = [
-        { name: 'Rock Guitar', url: 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?1716174521095' }, // URL funcional para Rock
-        { name: 'Jazz Guitar', url: 'https://server01.ouvir.radio.br:8006/stream?1716175071041' },
-        { name: 'Blues Guitar', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
-        { name: 'Heavy Metal Guitar', url: 'https://stm39.stmsrv.com:8382/;?1716176724313' },
+        { name: 'Rock', url: 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?1716174521095' }, // URL funcional para Rock
+        { name: 'Jazz', url: 'https://server01.ouvir.radio.br:8006/stream?1716175071041' },
+        { name: 'Blues', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
+        { name: 'Gospel', url: 'https://stm39.stmsrv.com:8382/;?1716176724313' },
         { name: 'Anos 80', url: 'https://live1.livemus.com.br:27400/stream?1716176895555' }
     ];
 
