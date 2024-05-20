bc22204c471002193cb3d5f40511ef69c04fa02d
portalguitarshred <portalguitarshred@gmail.com>
2024-05-20 00:19:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const stations = [
         { name: 'Rock Guitar', url: 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?1716174521095' }, // URL funcional para Rock
         { name: 'Jazz Guitar', url: 'https://server01.ouvir.radio.br:8006/stream?1716175071041' },
-        { name: 'Blues Guitar', url: 'https://stream-176.zeno.fm/d6dg4e0dytzuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJkNmRnNGUwZHl0enV2IiwiaG9zdCI6InN0cmVhbS0xNzYuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IlVVbmZmYXkyUmZPSzdCbmlUVFM3anciLCJpYXQiOjE3MTYxNzQ5MDUsImV4cCI6MTcxNjE3NDk2NX0.O8qECDCQ-9K5GskMS0gpkSa9sQIMLBX5CfThrNJ7h4M&1716174904649' },
+        { name: 'Blues Guitar', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
         { name: 'Heavy Metal Guitar', url: 'http://ice1.somafm.com/thistle-128-mp3' },
         { name: 'Instrumental Guitar', url: 'http://ice1.somafm.com/sf1033-128-mp3' }
     ];
