a61e605938048deefe7a349c7bce4f89a12501ee
portalguitarshred <portalguitarshred@gmail.com>
2024-05-20 00:33:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'Jazz Guitar', url: 'https://server01.ouvir.radio.br:8006/stream?1716175071041' },
         { name: 'Blues Guitar', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
         { name: 'Heavy Metal Guitar', url: 'http://66.70.187.30:8992/stream?1716175515235' },
-        { name: 'Instrumental Guitar', url: 'http://ice1.somafm.com/sf1033-128-mp3' }
+        { name: 'Gospel', url: 'https://8612.brasilstream.com.br/stream?1716175969400' }
     ];
 
     stations.forEach(station => {
