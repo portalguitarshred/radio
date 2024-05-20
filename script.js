ed3d8518a24abdb700faebc524e75cfcf177854c
portalguitarshred <portalguitarshred@gmail.com>
2024-05-20 06:54:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentPlaying = null;
 
     const stations = [
-        { name: 'Rock', url: 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?1716174521095' },
+        { name: 'Rock', url: 'https://stream.zeno.fm/qupiusi3w5puv' },
         { name: 'Jazz', url: 'https://server01.ouvir.radio.br:8006/stream?1716175071041' },
         { name: 'Blues', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
         { name: 'Metal', url: 'https://stm39.stmsrv.com:8382/;?1716176724313' },
