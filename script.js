232d80153839ce188d7cd214ec1d546f3e79e390
portalguitarshred <portalguitarshred@gmail.com>
2024-05-22 22:12:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'Classic Rocks', url: 'https://stream.zeno.fm/amepggt3jxptv' },
         { name: 'Blues', url: 'https://stream-172.zeno.fm/5npyxpydys8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI1bnB5eHB5ZHlzOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InhoZFV4UGNNUUc2eWsxR0I5ZzdENFEiLCJpYXQiOjE3MTYxNzUxMzksImV4cCI6MTcxNjE3NTE5OX0.-H5QPR8KYpGEe4-i0vuClifNYM0sWZzNvkTHhRqPUuM&1716175139266' },
         { name: 'Metal', url: 'https://stm39.stmsrv.com:8382/;?1716176724313' },
-        { name: 'Classic Rocks', url: 'https://stream.zeno.fm/amepggt3jxptv' },
         { name: 'Anos 80', url: 'https://stream-158.zeno.fm/3ywickpd3rkvv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiIzeXdpY2twZDNya3Z2IiwiaG9zdCI6InN0cmVhbS0xNTguemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IjVNUzljTlY0VG02VWlBZFVvazBqcFEiLCJpYXQiOjE3MTYxNzg2ODcsImV4cCI6MTcxNjE3ODc0N30.Umsbo62LR5tbHfFHYA63nvU1B6z38tBmwLqOZ07L50c&1716178687366' }
     ];
 
